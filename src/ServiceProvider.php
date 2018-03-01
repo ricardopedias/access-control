c8516865c6c5bbb7b3d93fa8b331e1b3c2cca2a2
Ricardo Pereira <rpdesignerfly@gmail.com>
2018-03-01 12:09:50 -0300

    Adicionados os publicadores adicionais para as views dos botões e dos CRUDs

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -20,22 +20,19 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'laracl');
         
-        // Config
-        // php artisan vendor:publish
-        $this->publishes([__DIR__.'/config/laracl.php' => config_path('laracl.php')], 'config-laracl');
-        
-        // Views
-        // php artisan vendor:publish --provider="Laracl\ServiceProvider"
-        //$this->publishes([__DIR__.'/resources/views' => resource_path('views/plexi/foundation')]);
-        
-        // Assets
-        //$this->publishes([__DIR__.'/public' => public_path('plexi/foundation')]);
-        
-        // Alternativa agrupada em 'public' (pode ser qualquer palavra)
-        // php artisan vendor:publish --tag=public --force
-        //$this->publishes([__DIR__.'/public' => public_path('plexi/foundation')], 'public');
+        // php artisan vendor:publish --tag=laracl-config
+        $this->publishes([__DIR__.'/config/laracl.php' => config_path('laracl.php')], 'laracl-config');
 
-        //$roles_list = \App::runningInConsole() == true ? [] : config('laracl.roles');
+        // php artisan vendor:publish --tag=laracl-buttons
+        $this->publishes([__DIR__.'/resources/views/buttons' => resource_path('views/laracl/buttons')], 'laracl-buttons');
+
+        // php artisan vendor:publish --tag=laracl-cruds
+        $this->publishes([__DIR__.'/resources/views/users' => resource_path('views/laracl/cruds/users')], 'laracl-cruds');
+        $this->publishes([__DIR__.'/resources/views/users-permissions' => resource_path('views/laracl/cruds/users-permissions')], 'laracl-cruds');
+        $this->publishes([__DIR__.'/resources/views/groups' => resource_path('views/laracl/cruds/groups')], 'laracl-cruds');
+        $this->publishes([__DIR__.'/resources/views/groups-permissions' => resource_path('views/laracl/cruds/groups-permissions')], 'laracl-cruds');
+        $this->publishes([__DIR__.'/resources/views/messages' => resource_path('views/laracl/cruds/messages')], 'laracl-cruds');
+        $this->publishes([__DIR__.'/resources/views/document.blade.php' => resource_path('views/laracl/cruds/document.blade.php')], 'laracl-cruds');
 
         \Laracl::registerPolicies();
 
@@ -53,10 +50,25 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/config/laracl.php', 'laracl');
         
         $this->normalizeConfig();
-
-        //$this->app->make('Plexi\Foundation\Http\Controllers\ExampleController');
     }
 
+    /**
+     * Gera a estrutura para nomeamento de rotas para os CRUDs, 
+     * com base nas urls especificadas na configuração.
+     * 
+     * Por exemplo, no item ['users' => 'painel/usuarios'], 
+     * serão extraidos os indices e os nomes para as rotas dos CRUDs, 
+     * ficando assim:
+     * [
+     *     laracl.routes.users.base  =>  users
+     *     laracl.routes.users.index  => usuarios.index
+     *     laracl.routes.users.create => usuarios.create
+     *     laracl.routes.users.store  => usuarios.store
+     *     laracl.routes.users.edit   => usuarios.edit
+     *     laracl.routes.users.update => usuarios.update
+     *     laracl.routes.users.delete => usuarios.delete
+     * ]
+     */
     protected function normalizeConfig()
     {
         $config = config('laracl');
