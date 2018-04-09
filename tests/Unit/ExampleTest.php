ce0f7f5493bfb48f14b412e328340770f192ba52
Ricardo Pereira <rpdesignerfly@gmail.com>
2018-04-09 19:52:12 -0300

    Adicionado suporte para testes de unidade e funcionalidade

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Laracl\Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExampleTest extends TestCase
+{
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
