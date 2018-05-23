67b2451b17da74a0e9d35ff058111b92045abf02
Ricardo Pereira <rpdesignerfly@gmail.com>
2018-05-23 17:32:50 -0300

    Correção no nome da tabela

--- src/Models/AclUserGroup.php
+++ src/Models/AclUserGroup.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AclUserGroup extends Model
 {
-    protected $table = 'acl_users_relations';
+    protected $table = 'acl_users_groups';
 
     protected $primaryKey = 'user_id';
 
