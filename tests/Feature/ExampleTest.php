2937e629ec69b756c69cabf92208dd258775af1c
Ricardo Pereira <rpdesignerfly@gmail.com>
2018-05-28 13:17:09 -0300

    Criado teste de unidade para o formulário de criação de usuários

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -20,7 +20,39 @@ class ExampleTest extends IControllerTestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testUserCreateTest()
+    {
+        self::createGroup();
+
+        // $this->withoutMiddleware();
+
+        $user = \App\User::find(1);
+        $this->actingAs($user);
+
+        $response = $this->get('/laracl/users');
+        $response->assertStatus(200);
+
+        $response = $this->get('/laracl/users/create');
+        $response->assertStatus(200);
+
+        $faker = \Faker\Factory::create();
+        $user_email = $faker->unique()->safeEmail;
+        $response = $this->post('/laracl/users/store', [
+            'name'         => $faker->name,
+            'email'        => $user_email,
+            'password'     => bcrypt('secret'),
+            'acl_group_id' => 1
+        ]);
+        $user = \App\User::where('email', $user_email)->first();
+
+        dd($user);
+
+
+        $response->assertRedirect("/laracl/users/edit/" . $user->id);
+        $response->assertStatus(200);
+    }
+
+    public function testUserEditTest()
     {
         // $this->withoutMiddleware();
 
@@ -33,8 +65,8 @@ class ExampleTest extends IControllerTestCase
         $response = $this->get('/laracl/users/create');
         $response->assertStatus(200);
 
-        // $response = $this->get('/laracl/users/edit');
-        // $response->assertStatus(200);
+        /*$response = $this->get('/laracl/users/edit', [ 'id' => 1]);
+        $response->assertStatus(200);*/
 
         //dd('xxx');
     }
