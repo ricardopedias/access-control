415c94cd87c6fb6f73478577014ac5216651c866
Ricardo Pereira <rpdesignerfly@gmail.com>
2018-06-06 14:33:21 -0300

    Atualização da visão que rendediza os botões 'delete'

--- src/resources/views/buttons/delete.blade.php
+++ src/resources/views/buttons/delete.blade.php
@@ -10,48 +10,67 @@
 @else
 
     @php
-        $btn_id = 'delete_' . md5($url);
-        if (!isset($btns)) {
-            $btns = [];
-        }
-        if (!isset($btns[$btn_id])) {
-            $btns[$btn_id] = 1;
-            $create_modal = true;
-        } else {
-            $create_modal = false;
-        }
-
+        $btn_id = 'btn-delete-' . md5(microtime());
     @endphp
 
-    <a href="{{ $url }}" class="btn btn-danger {{ $size != 'none' ? "btn-{$size}" : '' }}"
-       data-toggle="modal" data-target="#{{ $btn_id }}"
-       title="{{ $label }}">
+    <a id="{{ $btn_id }}" href="javascript:void(0)"
+       class="btn btn-danger {{ $size != 'none' ? "btn-{$size}" : '' }} acl-action-delete"
+       data-url="{{ $url }}" title="{{ $label }}">
         <i class="{{ $icon or 'fas fa-times' }}"></i>
         <span class="d-none d-lg-inline">{{ $label }}</span>
     </a>
 
-    <!-- Modal -->
-    <div class="modal fade" id="{{ $btn_id }}" tabindex="-1" role="dialog" aria-labelledby="{{ $btn_id }}Label" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Exclusão de Registro</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">
-                        <i class="{{ $icon or 'fas fa-times' }}"></i>
-                        Excluir
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
+    {{--
+    O Código abaixo será renderizado apenas uma vez,
+    quando o primeiro botão do tipo delete for invocado.
+    --}}
+    @php
+        global $acl_delete_confirm_modal;
+
+        if(isset($acl_delete_confirm_modal)) {
+            $acl_delete_confirm_modal = false;
+        } else {
+            $acl_delete_confirm_modal = true;
+        }
+    @endphp
+
+    @if($acl_delete_confirm_modal == true)
+
+        @include('laracl::modal-delete')
+
+        <script>
+
+            /*
+            Ao final do carregamento do documento, instancia o objeto AclConfirmDelete
+            e aplica o evento de clique em todos os botões 'delete'.
+            O AclConfirmDelete é carregado em laracl/src/resources/views/modal-delete.blade.php
+            */
+
+            function acl_attach_confirm(elem) {
+                var confirm = new AclConfirmDelete();
+                confirm.debugMode({{ var_export(env('APP_DEBUG') || env('APP_ENV') === 'local') }});
+                confirm.setToken('{{ csrf_token() }}');
+                confirm.removeGridRow('{{ $delete_row }}');
+                confirm.attach(elem);
+            }
+
+            if (undefined === window.$) {
+                // Se jQuery ainda não estiver carregado
+                window.onload = function(){
+                    $('.acl-action-delete').each(function(){
+                        acl_attach_confirm(this);
+                    });
+                };
+            } else {
+                // se jQuery já estiver disponível
+                $(document).ready(function(){
+                    $('.acl-action-delete').each(function(){
+                        acl_attach_confirm(this);
+                    });
+                });
+            }
+
+        </script>
+    @endif
 
 @endif
